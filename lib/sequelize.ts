fab517955bd7a621dd8f91f9927db19b7b68b7fa
adrianl01 <gustavo.adrian.leiva879@gmail.com>
2025-02-28 23:25:38 -0300

    4th commit

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -4,9 +4,8 @@ const sequelize = new Sequelize(process.env.SEQUELIZE_URI as any, {
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            rejectUnauthorized: false
         },
-
     },
-}) // Example for postgres
+})
 export { sequelize }
