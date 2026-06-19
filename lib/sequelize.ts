92a072657f7ac60344ad33c6bb32707a482fb2e2
adrianl01 <ga.leiva.1601@gmail.com>
2026-06-19 14:44:08 -0300

    Update SSL certificate handling in Sequelize configuration

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,16 +1,13 @@
-import fs from "fs";
-import { Sequelize } from "sequelize";
-import pg from "pg";
+import fs from 'fs';
+import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
-export const sequelize = new Sequelize(
-  process.env.SEQUELIZE_URI!,
-  {
-    dialectModule: pg,
-    dialectOptions: {
-      ssl: {
-        ca: fs.readFileSync("./certs/ca.pem").toString(),
-        rejectUnauthorized: true,
-      },
-    },
+export const sequelize = new Sequelize(process.env.SEQUELIZE_URI!, {
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      ca: process.env.AIVEN_CA_CERT,
+      rejectUnauthorized: true
+    }
   }
-);
+});
